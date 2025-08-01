6adbb7348eec3482549758fc131e4d4ae600be32
mallah204 <kashifrazamallah204@gmail.com>
2025-08-01 08:56:32 +0500

    Update autosticker.js

--- Rudra/commands/autosticker.js
+++ Rudra/commands/autosticker.js
@@ -16,17 +16,17 @@ module.exports.handleEvent = async function ({ api, event }) {
   // Stickers from Piyomaru Playground, QooBee Gets Funny, Milk & Mocha: Cutest!
   const stickers = [
     // Piyomaru Playground
-    "994458070956118",
-    "994457834289475",
-    "994457597622832",
+    "7533460264934895089",
+    "7533459305567939100",
+    "7533460316293063094",
     // QooBee Gets Funny
-    "958598070838103",
-    "958598314171412",
-    "958598470838063",
+    "7533458903425173001",
+    "7533459765367118492",
+    "",
     // Milk & Mocha: Cutest!
-    "909175329597154",
-    "909175389597148",
-    "909175269597160"
+    "",
+    "",
+    ""
   ];
 
   const randomSticker = stickers[Math.floor(Math.random() * stickers.length)];
